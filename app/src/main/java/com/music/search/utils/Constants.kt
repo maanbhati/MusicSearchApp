bfca4cc9402952c7d1d0a34b60faca2b76e7babb
Mansingh Bhati <maanbhati@gmail.com>
2021-07-11 12:16:23 +0100

    Minor changes

--- app/src/main/java/com/music/search/utils/Constants.kt
+++ app/src/main/java/com/music/search/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.music.search.utils
 
-const val BASE_SEARCH_URL = "https://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=f9dc6ff0777b0eed89f0bc685d703945&format=json/"
+const val BASE_SEARCH_URL = "https://ws.audioscrobbler.com/2.0/?method=album.search&album=believe&api_key=&format=json/"
 const val URL_ALBUM = "&album="
 const val SEARCH_KEYWORD = ""
 const val API_KEY = "&api_key="
